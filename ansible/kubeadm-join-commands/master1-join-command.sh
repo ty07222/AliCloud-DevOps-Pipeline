582b2aafbf5daff669331b1dcf28a3b4970697aa
ty07222 <2592636375@qq.com>
2025-10-06 11:54:24 +0800

    Delete ansible/kubeadm-join-commands directory

--- ansible/kubeadm-join-commands/master1-join-command.sh
+++
@@ -1,6 +0,0 @@
-# Kubernetes join command for master1
-# Generated at 2025-09-20T07:37:02Z
-kubeadm join 172.16.5.64:6443 --token 1k4473.fbwhmt1ucoffwy9n --discovery-token-ca-cert-hash sha256:1c455555b17f46e29ac609c4aafce371491b0699ac9cdb86acff9a5b55d5bbf1
-
-# 使用示例：
-# kubeadm join 172.16.5.64:6443 --token 1k4473.fbwhmt1ucoffwy9n --discovery-token-ca-cert-hash sha256:1c455555b17f46e29ac609c4aafce371491b0699ac9cdb86acff9a5b55d5bbf1
