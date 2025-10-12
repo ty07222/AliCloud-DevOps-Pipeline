ea2c072de4b21c51b79deb51adf578a34797bfd6
ty07222 <2592636375@qq.com>
2025-10-12 16:25:33 +0800

    Update main.tf

--- terraform-aliyun-ecs/modules/ecs-instance/main.tf
+++ terraform-aliyun-ecs/modules/ecs-instance/main.tf
@@ -2,17 +2,14 @@ data "alicloud_instance_types" "this" {
   cpu_core_count = 2
   memory_size    = 4
   availability_zone = var.availability_zone
-  # 可以根据需要添加其他过滤器，如实例系列
 }
 
-# 查询镜像，例如选择一种常见的Linux镜像
 data "alicloud_images" "this" {
-  name_regex  = "^ubuntu_18.*64" # 以 Ubuntu 18.04 为例，可按需修改
+  name_regex  = "^ubuntu_18.*64"
   most_recent = true
   owners      = "system"
 }
 
-# 创建ECS实例
 resource "alicloud_instance" "this" {
   instance_name              = var.instance_name
   instance_type              = data.alicloud_instance_types.this.instance_types.0.id
